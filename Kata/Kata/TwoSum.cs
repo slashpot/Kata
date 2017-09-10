0343f928fd65e102d672badfa51012127fc625d0
Stan Lin <ttttt123465@gmail.com>
2017-09-10 13:11:42 +0800

    add TwoSum

---
+++ Kata/Kata/TwoSum.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+
+namespace Kata
+{
+    public class TwoSum
+    {
+        public static int[] GetTwoSum(int[] nums, int target)
+        {
+            int[] result = new int[2];
+            Dictionary<int, int> records = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int current = nums[i];
+                int another = target - current;
+
+                if (records.ContainsKey(another))
+                {
+                    result[0] = records[another];
+                    result[1] = i;
+                    break;
+                }
+                if (!records.ContainsKey(current))
+                    records.Add(current, i);
+            }
+            return result;
+        }
+    }
+}
---
+++ Kata/Kata_test/TwoSum_test.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using Kata;
+
+namespace Kata_test
+{
+    [TestFixture]
+    public class TwoSum_test
+    {
+        [TestCase(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+        [TestCase(new[] { -3, 4, 3, 90 }, 0, new[] { 0, 2 })]
+        public void TestMethod1(int[] nums, int target, int[] expected)
+        {
+            int[] actual = TwoSum.GetTwoSum(nums, target);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
