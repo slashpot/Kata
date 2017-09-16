9c3fb7848730c23d43cac5239779a8ead8c24872
Stan Lin <ttttt123465@gmail.com>
2017-09-16 19:41:24 +0800

    add ThreeSum

---
+++ Kata/Kata/ThreeSum.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kata
+{
+    public class ThreeSum
+    {
+        public IList<IList<int>> GetThreeSum(int[] nums)
+        {
+            List<IList<int>> result = new List<IList<int>>();
+            if (nums.Length < 3) return result;
+
+            Array.Sort(nums);
+            int startPos = 0;
+
+            while (startPos < nums.Length - 1)
+            {
+                int checkPos = startPos + 1;
+
+                while(checkPos < nums.Length)
+                {
+                    int first = nums[startPos];
+                    int second = nums[checkPos];
+                    int complement = (first + second) * -1;
+                    int nextPos = checkPos + 1;
+
+                    while (checkPos < nums.Length - 1 && nums[checkPos] == nums[checkPos + 1])
+                        checkPos++;
+                    checkPos++;
+
+                    if (Array.BinarySearch(nums, nextPos, nums.Length - nextPos, complement) < 0)
+                        continue;
+                    
+                    result.Add(new List<int> { first, second, complement });
+                }
+
+                while (startPos < nums.Length - 1 && nums[startPos] == nums[startPos + 1])
+                    startPos++;
+                startPos++;
+            }
+            return result;
+        }
+    }
+}
---
+++ Kata/Kata_test/ThreeSum_test.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+using Kata;
+
+namespace Kata_test
+{
+    [TestFixture]
+    public class ThreeSum_test
+    {
+        [Test]
+        public void TestCase1()
+        {
+            ThreeSum threeSum = new ThreeSum();
+            int[] nums = new int[6] { -1, 0, 1, 2, -1, -4 };
+
+            IList<IList<int>> expected = new List<IList<int>>
+            {
+                new List<int> {-1, -1, 2 },
+                new List<int> {-1, 0, 1 }
+            };
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            ThreeSum threeSum = new ThreeSum();
+            int[] nums = new int[2] { 0, 0 };
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            ThreeSum threeSum = new ThreeSum();
+            int[] nums = new int[6] {3, 0, -2, -1, 1, 2};
+
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int> { -2, -1, 3 },
+                new List<int> { -2, 0, 2 },
+                new List<int> { -1, 0, 1}
+            };
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            ThreeSum threeSum = new ThreeSum();
+            int[] nums = new int[3] { 0, 0, 0 };
+
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int> { 0, 0, 0 },
+            };
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase5()
+        {
+            ThreeSum threeSum = new ThreeSum();
+            int[] nums = new int[3] { 0, 1, 1 };
+
+            IList<IList<int>> expected = new List<IList<int>>();
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+
+            Assert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void TestCase6()
+        {
+            ThreeSum threeSum = new ThreeSum();
+            int[] nums = new int[15] { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 };
+
+            IList<IList<int>> expected = new List<IList<int>>()
+            {
+                new List<int> { -4, -2, 6},
+                new List<int> { -4, 0, 4},
+                new List<int> { -4, 1, 3},
+                new List<int> { -4, 2, 2},
+                new List<int> { -2, -2, 4},
+                new List<int> { -2, 0, 2},
+            };
+
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
