86223e236f7b11acea0af8c208cbebbdbe5e9b4f
Stan Lin <ttttt123465@gmail.com>
2017-09-26 00:02:36 +0800

    finish NextBiggerNumberWithTheSameDigits

---
+++ Kata/Kata/NextBiggerNumberWithTheSameDigits.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace Kata
+{
+    public class NextBiggerNumberWithTheSameDigits
+    {
+        public static long NextBiggerNumber(long n)
+        {
+            var digits = ConvertInputToArray(n);
+            var searchIndex = digits.Length - 1;
+
+            while (searchIndex != 0)
+            {
+                var previousDigit = digits[searchIndex - 1];
+
+                if (digits[searchIndex] <= previousDigit)
+                {
+                    searchIndex--;
+                }
+                else
+                {
+                    Array.Sort(digits, searchIndex, digits.Length - searchIndex);
+                    for (int i = searchIndex; i < digits.Length; i++)
+                    {
+                        if (digits[i] > previousDigit)
+                        {
+                            var temp = previousDigit;
+                            digits[searchIndex - 1] = digits[i];
+                            digits[i] = temp;
+                            break;
+                        }
+                    }
+
+                    return long.Parse(string.Join("", digits));
+                }
+            }
+
+            return -1;
+        }
+
+        private static int[] ConvertInputToArray(long n)
+        {
+            return n.ToString().Select(t => int.Parse(t.ToString())).ToArray();
+        }
+    }
+}
---
+++ Kata/Kata_test/NextBiggerNumberWithTheSameDigits_test.cs
@@ -0,0 +1,30 @@
+﻿
+
+using Kata;
+using NUnit.Framework;
+
+namespace Kata_test
+{
+    [TestFixture]
+    public class NextBiggerNumberWithTheSameDigits_test
+    {
+        [TestCase(12, 21)]
+        [TestCase(513, 531)]
+        [TestCase(2017, 2071)]
+        [TestCase(414, 441)]
+        [TestCase(144, 414)]
+        [TestCase(59884848459853, 59884848483559)]
+        public void HasBiggerNumber(long input, long expected)
+        {
+            Assert.AreEqual(expected, NextBiggerNumberWithTheSameDigits.NextBiggerNumber(input));
+        }
+
+        [TestCase(9)]
+        [TestCase(111)]
+        [TestCase(531)]
+        public void HasNoBiggerNumber(long input)
+        {
+            Assert.AreEqual(-1, NextBiggerNumberWithTheSameDigits.NextBiggerNumber(input));
+        }
+    }
+}
