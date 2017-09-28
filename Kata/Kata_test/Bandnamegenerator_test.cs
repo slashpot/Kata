f44be98b41d7527e4f1b2f11a5691daecf834bd5
書勤 林 <ttttt123465@gmail.com>
2017-09-28 17:00:15 +0800

    remove redundent namespaces

--- Kata/Kata_test/Bandnamegenerator_test.cs
+++ Kata/Kata_test/Bandnamegenerator_test.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Kata;
+﻿using Kata;
 using NUnit.Framework;
-using NUnit.Framework.Internal;
 using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
 
 namespace Kata_test
