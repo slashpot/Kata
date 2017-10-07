b145cec5abe06c6abbbf5fc26d10649adc74ee8e
Stan Lin <ttttt123465@gmail.com>
2017-10-07 10:40:12 +0800

    refactor ThreeSum

--- Kata/Kata/ThreeSum.cs
+++ Kata/Kata/ThreeSum.cs
@@ -5,40 +5,106 @@ namespace Kata
 {
     public class ThreeSum
     {
+        private List<IList<int>> _threeSumNums;
+        private int _startPos;
+        private int[] _inputNums;
+
         public IList<IList<int>> GetThreeSum(int[] nums)
         {
-            List<IList<int>> result = new List<IList<int>>();
-            if (nums.Length < 3) return result;
+            Initialize(nums);
 
-            Array.Sort(nums);
-            int startPos = 0;
-
-            while (startPos < nums.Length - 1)
+            if (IsInputCountSmallerThanThree())
             {
-                int checkPos = startPos + 1;
+                return _threeSumNums;
+            }
 
-                while(checkPos < nums.Length)
+            SortInput();
+            GetThreeSumNums();
+            return _threeSumNums;
+        }
+
+        private void Initialize(int[] nums)
+        {
+            _inputNums = nums;
+            _threeSumNums = new List<IList<int>>();
+            _startPos = 0;
+        }
+
+        private bool IsInputCountSmallerThanThree()
+        {
+            return _inputNums.Length < 3;
+        }
+
+        private void SortInput()
+        {
+            Array.Sort(_inputNums);
+        }
+
+        private void GetThreeSumNums()
+        {
+            while (HasUncheckedPos())
+            {
+                FindThreeSumLists();
+                _startPos = SkipSameNumsToNext(_startPos);
+            }
+        }
+
+        private bool HasUncheckedPos()
+        {
+            return _startPos < _inputNums.Length - 1;
+        }
+
+        private void FindThreeSumLists()
+        {
+            int checkPos = _startPos + 1;
+
+            while (checkPos < _inputNums.Length)
+            {
+                int firstThreeSumNum = GetFirstThreeSumNum();
+                int secondThreeSumNum = GetSecondThreeSumNum(checkPos);
+                int complement = GetComplement(firstThreeSumNum, secondThreeSumNum);
+                int nextPos = checkPos + 1;
+                checkPos = SkipSameNumsToNext(checkPos);
+
+                if (HasNoComplementInRemainNums(nextPos, complement))
                 {
-                    int first = nums[startPos];
-                    int second = nums[checkPos];
-                    int complement = (first + second) * -1;
-                    int nextPos = checkPos + 1;
-
-                    while (checkPos < nums.Length - 1 && nums[checkPos] == nums[checkPos + 1])
-                        checkPos++;
-                    checkPos++;
-
-                    if (Array.BinarySearch(nums, nextPos, nums.Length - nextPos, complement) < 0)
-                        continue;
-                    
-                    result.Add(new List<int> { first, second, complement });
+                    continue;
                 }
 
-                while (startPos < nums.Length - 1 && nums[startPos] == nums[startPos + 1])
-                    startPos++;
-                startPos++;
+                AddThreeSumNums(firstThreeSumNum, secondThreeSumNum, complement);
             }
-            return result;
+        }
+
+        private int GetFirstThreeSumNum()
+        {
+            return _inputNums[_startPos];
+        }
+
+        private int GetSecondThreeSumNum(int checkPos)
+        {
+            return _inputNums[checkPos];
+        }
+
+        private static int GetComplement(int firstThreeSumNum, int secondThreeSumNum)
+        {
+            return (firstThreeSumNum + secondThreeSumNum) * -1;
+        }
+
+        private int SkipSameNumsToNext(int checkPos)
+        {
+            while (checkPos < _inputNums.Length - 1 && _inputNums[checkPos] == _inputNums[checkPos + 1])
+                checkPos++;
+            return checkPos + 1;
+        }
+
+        private bool HasNoComplementInRemainNums(int nextPos, int complement)
+        {
+            return Array.BinarySearch(_inputNums, nextPos, _inputNums.Length - nextPos, complement) < 0;
+        }
+
+        private void AddThreeSumNums(int first, int second, int complement)
+        {
+            _threeSumNums.Add(new List<int> { first, second, complement });
         }
     }
 }
--- Kata/Kata_test/ThreeSum_test.cs
+++ Kata/Kata_test/ThreeSum_test.cs
@@ -1,90 +1,76 @@
-﻿using System.Collections.Generic;
+﻿using Kata;
 using NUnit.Framework;
-using Kata;
+using System.Collections.Generic;
 
 namespace Kata_test
 {
     [TestFixture]
     public class ThreeSum_test
     {
+        private static void TestThreeSum(int[] nums, IList<IList<int>> expected)
+        {
+            ThreeSum threeSum = new ThreeSum();
+            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
+            Assert.AreEqual(expected, actual);
+        }
+
         [Test]
         public void TestCase1()
         {
-            ThreeSum threeSum = new ThreeSum();
             int[] nums = new int[6] { -1, 0, 1, 2, -1, -4 };
-
             IList<IList<int>> expected = new List<IList<int>>
             {
                 new List<int> {-1, -1, 2 },
                 new List<int> {-1, 0, 1 }
             };
-            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
-
-            Assert.AreEqual(expected, actual);
+            TestThreeSum(nums, expected);
         }
 
         [Test]
         public void TestCase2()
         {
-            ThreeSum threeSum = new ThreeSum();
             int[] nums = new int[2] { 0, 0 };
-
             IList<IList<int>> expected = new List<IList<int>>();
-            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
-
-            Assert.AreEqual(expected, actual);
+            TestThreeSum(nums, expected);
         }
 
         [Test]
         public void TestCase3()
         {
-            ThreeSum threeSum = new ThreeSum();
-            int[] nums = new int[6] {3, 0, -2, -1, 1, 2};
-
+            int[] nums = new int[6] { 3, 0, -2, -1, 1, 2 };
             IList<IList<int>> expected = new List<IList<int>>()
             {
                 new List<int> { -2, -1, 3 },
                 new List<int> { -2, 0, 2 },
                 new List<int> { -1, 0, 1}
             };
-            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
-
-            Assert.AreEqual(expected, actual);
+            TestThreeSum(nums, expected);
         }
 
         [Test]
         public void TestCase4()
         {
-            ThreeSum threeSum = new ThreeSum();
             int[] nums = new int[3] { 0, 0, 0 };
 
             IList<IList<int>> expected = new List<IList<int>>()
             {
                 new List<int> { 0, 0, 0 },
             };
-            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
-
-            Assert.AreEqual(expected, actual);
+            TestThreeSum(nums, expected);
         }
 
         [Test]
         public void TestCase5()
         {
-            ThreeSum threeSum = new ThreeSum();
             int[] nums = new int[3] { 0, 1, 1 };
-
             IList<IList<int>> expected = new List<IList<int>>();
-            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
-
-            Assert.AreEqual(expected, actual);
+            TestThreeSum(nums, expected);
         }
 
         [Test]
         public void TestCase6()
         {
-            ThreeSum threeSum = new ThreeSum();
             int[] nums = new int[15] { -4, -2, -2, -2, 0, 1, 2, 2, 2, 3, 3, 4, 4, 6, 6 };
-
             IList<IList<int>> expected = new List<IList<int>>()
             {
                 new List<int> { -4, -2, 6},
@@ -94,9 +80,7 @@ namespace Kata_test
                 new List<int> { -2, -2, 4},
                 new List<int> { -2, 0, 2},
             };
-
-            IList<IList<int>> actual = threeSum.GetThreeSum(nums);
-            Assert.AreEqual(expected, actual);
+            TestThreeSum(nums, expected);
         }
     }
 }
