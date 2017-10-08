9fe29b11e255177c9b2d703af05785cf6b0cefaa
Stan Lin <ttttt123465@gmail.com>
2017-10-08 14:00:34 +0800

    move some classes to Leetcode

---
+++ Kata/Kata/SumStringsAsNumbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata
+{
+    public class SumStringsAsNumbers    
+    {
+        public static string sumStrings(string a, string b)
+        {
+            long firstNum = Convert.ToInt64(a);
+            long secondNum = Convert.ToInt64(b);
+            long sum = firstNum + secondNum;
+            return sum.ToString();
+        }
+    }
+}
---
+++ Kata/Kata_test/SumStringsAsNumbers_test.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Kata;
+using NUnit.Framework;
+
+namespace Kata_test
+{
+    [TestFixture]
+    public class SumStringsAsNumbers_test
+    {
+        [Test]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("579", SumStringsAsNumbers.sumStrings("123", "456"));
+        }
+    }
+}
