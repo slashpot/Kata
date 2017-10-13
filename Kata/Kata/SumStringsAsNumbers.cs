35cbcf6c47c8653f3abed0cd4a83a58884934c13
書勤 林 <ttttt123465@gmail.com>
2017-10-13 20:12:18 +0800

    developinh

--- Kata/Kata/SumStringsAsNumbers.cs
+++ Kata/Kata/SumStringsAsNumbers.cs
@@ -2,54 +2,55 @@
 {
     public class SumStringsAsNumbers
     {
-        private string _finalResult = "";
-        private bool _hasCarry = false;
-
-        public string sumStrings(string a, string b)
+        public static string sumStrings(string a, string b)
         {
-            AddCharToFinalResult(a, b);
-            return _finalResult;
+            return GetFinalResult(a, b);
         }
 
-        private void AddCharToFinalResult(string a, string b)
+        private static string GetFinalResult(string a, string b)
+        {
+            var finalResult = "";
+            AddCharToFinalResult(a, b, false, finalResult);
+            return finalResult;
+        }
+
+        private static void AddCharToFinalResult(string a, string b, bool hasParry, string finalResult)
         {
             var firstNum = a[a.Length - 1] - 48;
             var secondNum = b[b.Length - 1] - 48;
-
             var sum = firstNum + secondNum;
-            if (_hasCarry)
+
+            if (hasParry)
             {
                 sum++;
-                _hasCarry = false;
             }
+
+            var nextBitHasParry = false;
+
             if (sum >= 10)
             {
-                _hasCarry = true;
+                nextBitHasParry = true;
                 sum -= 10;
             }
 
-            _finalResult = sum + _finalResult;
+            finalResult = sum + finalResult;
 
-            var substringOfA = a.Substring(0, a.Length - 1);
-            var substringOfB = b.Substring(0, b.Length - 1);
-
-            if (substringOfA.Equals(""))
+            if (a.Length == 1)
             {
-                if (_hasCarry)
-                    AddCharToFinalResult(substringOfB, "1");
-                _finalResult = substringOfB + _finalResult;
+                var substringOfB = b.Substring(0, b.Length - 1);
+                AddCharToFinalResult(substringOfB, "1", nextBitHasParry, finalResult);
                 return;
             }
 
-            if (substringOfB.Equals(""))
+            if (b.Length == 1)
             {
-                if (_hasCarry)
-                    AddCharToFinalResult(substringOfA, "1");
-                _finalResult = substringOfA + _finalResult;
+                var substringOfA = a.Substring(0, a.Length - 1);
+                AddCharToFinalResult(substringOfA, "1", nextBitHasParry, finalResult);
                 return;
             }
 
-            AddCharToFinalResult(substringOfA, substringOfB);
+            AddCharToFinalResult();
+
         }
     }
 }
