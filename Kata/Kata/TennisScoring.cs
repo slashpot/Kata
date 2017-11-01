f28de3b98ddf76e5c78c54268e56964bad789a7a
Stan Lin <ttttt123465@gmail.com>
2017-11-01 23:10:29 +0800

    add and pass new tests

---
+++ Kata/Kata/TennisScoring.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+
+namespace Kata
+{
+    public class TennisScoring
+    {
+        private int eric = 0;
+        private int joey = 0;
+
+        private Dictionary<int, string> scoreLookUp = new Dictionary<int, string>()
+        {
+            {0, "Love" },
+            {1, "Fifteen" },
+            {2, "Thirty" },
+            {3, "Forty" },
+        };
+
+        public string GetStatue()
+        {
+            if (eric == joey)
+            {
+                return scoreLookUp[eric] + " All";
+            }
+            else
+            {
+                return scoreLookUp[eric] + " " + scoreLookUp[joey];
+            }
+        }
+
+        public void EricGetPoint()
+        {
+            eric++;
+        }
+
+        public void JoeyGetPoint()
+        {
+            joey++;
+        }
+    }
+}
---
+++ Kata/Kata_test/TennisScoring_test.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Kata;
+using NUnit.Framework;
+
+namespace Kata_test
+{
+    [TestFixture]
+    public class TennisScoring_test
+    {
+        [Test]
+        public void GetStatus_Beginning_ReturnLoveAll()
+        {
+            TennisScoring tennisScoring = new TennisScoring();
+            Assert.AreEqual("Love All", tennisScoring.GetStatue());
+        }
+
+        [Test]
+        public void GetStatus_Eric1_Joey0_ReturnFifteenLove()
+        {
+            TennisScoring tennisScoring = new TennisScoring();
+            tennisScoring.EricGetPoint();
+            Assert.AreEqual("Fifteen Love", tennisScoring.GetStatue());
+        }
+
+        [Test]
+        public void GetStatus_Eric0_Joey1_ReturnLoveFifteen()
+        {
+            TennisScoring tennisScoring = new TennisScoring();
+            tennisScoring.JoeyGetPoint();
+            Assert.AreEqual("Love Fifteen", tennisScoring.GetStatue());
+        }
+
+        [Test]
+        public void GetStatus_Eric1_Joey1_ReturnFifteenAll()
+        {
+            TennisScoring tennisScoring = new TennisScoring();
+            tennisScoring.JoeyGetPoint();
+            tennisScoring.EricGetPoint();
+            Assert.AreEqual("Fifteen All", tennisScoring.GetStatue());
+        }
+
+
+    }
+
+}
