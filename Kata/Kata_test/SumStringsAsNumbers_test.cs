cfcea41e192a17fc6ee834c369cf687ded4247ac
Stan Lin <ttttt123465@gmail.com>
2017-11-18 22:10:08 +0800

    add new tests and pass

--- Kata/Kata_test/SumStringsAsNumbers_test.cs
+++ Kata/Kata_test/SumStringsAsNumbers_test.cs
@@ -6,29 +6,34 @@ namespace Kata_test
     [TestFixture]
     public class SumStringsAsNumbers_test
     {
+        [SetUp]
+        public void SetUp()
+        {
+            SumStringsAsNumbers.Initialize();
+        }
+
         [TestCase("0", "0", "0")]
         [TestCase("0", "1", "1")]
         [TestCase("1", "2", "3")]
         [TestCase("12", "34", "46")]
         [TestCase("123", "345", "468")]
-        public void SumString_InputsAreSameDigitsWithoutCarry_ReturnSum(string firstInput, string secondInput, string sum)
+        public void SumString_InputsAreSameDigitsWithoutCarry(string firstInput, string secondInput, string sum)
         {
-            var sumStringsAsNumbers = new SumStringsAsNumbers();
-            Assert.AreEqual(sum, sumStringsAsNumbers.SumStrings(firstInput, secondInput));
+            Assert.AreEqual(sum, SumStringsAsNumbers.SumStrings(firstInput, secondInput));
         }
 
         [TestCase("1", "23", "24")]
-        public void SumString_InputsAreDifferentDigitsWithoutCarry_ReturnSum(string firstInput, string secondInput, string sum)
+        [TestCase("12", "3465", "3477")]
+        public void SumString_InputsAreDifferentDigitsWithoutCarry(string firstInput, string secondInput, string sum)
         {
-            var sumStringsAsNumbers = new SumStringsAsNumbers();
-            Assert.AreEqual(sum, sumStringsAsNumbers.SumStrings(firstInput, secondInput));
+            Assert.AreEqual(sum, SumStringsAsNumbers.SumStrings(firstInput, secondInput));
         }
 
-        //[TestCase("8", "9", "17")]
-        //public void SumString_InputsAreSameDigitWithCarry_ReturnSum(string firstInput, string secondInput, string sum)
-        //{
-        //    var sumStringsAsNumbers = new SumStringsAsNumbers();
-        //    Assert.AreEqual(sum, sumStringsAsNumbers.SumStrings(firstInput, secondInput));
-        //}
+        [TestCase("548", "345", "893")]
+        [TestCase("942", "13795", "14737")]
+        public void SumString_WithCarryExceptFirstDigit(string firstInput, string secondInput, string sum)
+        {
+            Assert.AreEqual(sum, SumStringsAsNumbers.SumStrings(firstInput, secondInput));
+        }
     }
 }
