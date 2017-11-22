29793202ee99164ee44678a7aa5c17d380706427
Stan Lin <ttttt123465@gmail.com>
2017-11-22 23:32:05 +0800

    rewrite tennis

---
+++ Kata/TennisTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TennisTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TennisTest")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("0b149b29-8175-46a1-b86e-39db97ad5ebd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Kata/TennisTest/TennisTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Tennis;
+
+namespace TennisTest
+{
+    [TestFixture]
+    public class TennisTest
+    {
+        private TennisGame _tennisGame = new TennisGame();
+
+        [SetUp]
+        public void Initialize()
+        {
+            _tennisGame = new TennisGame();
+        }
+
+        [Test]
+        public void Love_All()
+        {
+            ScoreShouldBe("Love All");
+        }
+
+        [Test]
+        public void Fifteen_Love()
+        {
+            SetFirstPlayerPoint(1);
+            ScoreShouldBe("Fifteen Love");
+        }
+
+        private void ScoreShouldBe(string expected)
+        {
+            Assert.AreEqual(expected, _tennisGame.GetResult());
+        }
+
+        private void SetFirstPlayerPoint(int round)
+        {
+            for (int i = 0; i < round; i++)
+            {
+                _tennisGame.FirstPlayerGetPoint();
+            }
+        }
+    }
+}
