b196739eb089f3cddab2fd4e43bf5b6562ec21a5
Eric Lin <eric.lin88@TI03NB107>
2018-04-04 16:42:40 +0800

    finish anagram detection

---
+++ Kata/Codewars/AnagramDetection.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    public class AnagramDetection
+    {
+        public static bool IsAnagram(string test, string original)
+        {
+            var lowerTest = test.ToLower().OrderBy(x => x);
+            var lowerOrigional = original.ToLower().OrderBy(x => x);
+            return lowerTest.SequenceEqual(lowerOrigional);
+        }
+    }
+}
---
+++ Kata/Kata_test/AnagramDetectionTest.cs
@@ -0,0 +1,21 @@
+﻿using Codewars;
+using NUnit.Framework;
+
+namespace Kata_test
+{
+    /// <summary>
+    /// Summary description for AnagramDetectionTest
+    /// </summary>
+    [TestFixture]
+    public class AnagramDetectionTest
+    {
+        [TestCase("foefet", "toffee", ExpectedResult = true)]
+        [TestCase("Buckethead", "DeathCubeK", ExpectedResult = true)]
+        [TestCase("Twoo", "Woot", ExpectedResult = true)]
+        [TestCase("apple", "pale", ExpectedResult = false)]
+        public bool FixedTest(string test, string original)
+        {
+            return AnagramDetection.IsAnagram(test, original);
+        }
+    }
+}
