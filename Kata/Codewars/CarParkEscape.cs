3e50cb4762f6421c33ee3f83ddbf89743efd93c6
Eric Lin <ttttt123465@gmail.com>
2018-04-06 10:52:48 +0800

    finish car park escape

---
+++ Kata/Codewars/CarParkEscape.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Codewars
+{
+    public class CarParkEscape
+    {
+        private readonly List<string> _finalRoute = new List<string>();
+        private int _currentFloors;
+        private int _floors;
+        private int[,] _carpark;
+        private int _floorLength;
+
+        public string[] Escape(int[,] carpark)
+        {
+            Init(carpark);
+            FindStairOrExit(GetStartPos());
+            return _finalRoute.ToArray();
+        }
+
+        private void Init(int[,] carpark)
+        {
+            _carpark = carpark;
+            _floors = _carpark.GetLength(0);
+            _floorLength = _carpark.GetLength(1);
+        }
+
+        private int GetStartPos()
+        {
+            for (int i = 0; i < _floors; i++)
+            {
+                var pos = GetIndex(i, (int)CarParkType.Start);
+                if (pos != -1)
+                {
+                    return pos;
+                }
+                _currentFloors++;
+            }
+            throw new Exception("get start pos fail");
+        }
+
+        private int GetIndex(int floor, int target)
+        {
+            return Enumerable.Range(0, _floorLength).Select(x => _carpark[floor, x]).ToList().IndexOf(target);
+        }
+
+        private void FindStairOrExit(int pos)
+        {
+            if (IsLastFloor())
+            {
+                GoToExit(pos);
+            }
+            else
+            {
+                var stair = GoToStair(pos);
+                GoDownStairs(stair);
+                FindStairOrExit(stair);
+            }
+        }
+
+        private void GoDownStairs(int stair)
+        {
+            var downstairs = 0;
+            while (_carpark[_currentFloors, stair] == (int)CarParkType.Stair && _currentFloors != _floors - 1)
+            {
+                downstairs++;
+                _currentFloors++;
+            }
+            _finalRoute.Add("D" + downstairs);
+        }
+
+        private int GoToStair(int pos)
+        {
+            var stair = GetIndex(_currentFloors, (int)CarParkType.Stair);
+            var distance = Math.Abs(pos - stair);
+            var direction = pos > stair ? "L" : "R";
+
+            _finalRoute.Add(direction + distance);
+            return stair;
+        }
+
+        private void GoToExit(int pos)
+        {
+            var indexOfExit = _floorLength - 1;
+            if (pos == indexOfExit) return;
+            var distance = indexOfExit - pos;
+            _finalRoute.Add("R" + distance);
+        }
+
+        private bool IsLastFloor()
+        {
+            return _currentFloors == _floors - 1;
+        }
+    }
+
+    public enum CarParkType
+    {
+        Stair = 1,
+        Start = 2
+    }
+}
---
+++ Kata/Kata_test/CarParkEscapeTest.cs
@@ -0,0 +1,66 @@
+﻿using Codewars;
+using NUnit.Framework;
+
+namespace Kata_test
+{
+    [TestFixture]
+    public class CarParkEscapeTest
+    {
+        private CarParkEscape _car;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _car = new CarParkEscape();
+        }
+
+        [Test]
+        public void EscapeTwoFloors()
+        {
+            int[,] carpark = { { 1, 0, 0, 0, 2 },
+                { 0, 0, 0, 0, 0 } };
+            string[] result = { "L4", "D1", "R4" };
+            Assert.AreEqual(result, _car.Escape(carpark));
+        }
+
+        [Test]
+        public void HasTwoContinuousStairs()
+        {
+            int[,] carpark = { { 2, 0, 0, 1, 0 },
+                { 0, 0, 0, 1, 0 },
+                { 0, 0, 0, 0, 0 } };
+            string[] result = { "R3", "D2", "R1" };
+            Assert.AreEqual(result, _car.Escape(carpark));
+        }
+
+        [Test]
+        public void HasThreeContinuousStairs()
+        {
+            int[,] carpark = { { 0, 2, 0, 0, 1 },
+                { 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 1 },
+                { 0, 0, 0, 0, 0 } };
+            string[] result = { "R3", "D3" };
+            Assert.AreEqual(result, _car.Escape(carpark));
+        }
+
+        [Test]
+        public void StairsFarWawy()
+        {
+            int[,] carpark = { { 1, 0, 0, 0, 2 },
+                { 0, 0, 0, 0, 1 },
+                { 1, 0, 0, 0, 0 },
+                { 0, 0, 0, 0, 0 } };
+            string[] result = { "L4", "D1", "R4", "D1", "L4", "D1", "R4" };
+            Assert.AreEqual(result, _car.Escape(carpark));
+        }
+
+        [Test]
+        public void AtExit()
+        {
+            int[,] carpark = { { 0, 0, 0, 0, 2 } };
+            string[] result = { };
+            Assert.AreEqual(result, _car.Escape(carpark));
+        }
+    }
+}
