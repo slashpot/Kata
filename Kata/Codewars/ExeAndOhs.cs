e52093b4224e32ab07cb21ba081959bbc67fa388
Eric Lin Shu Chin <eric.lin@titansoft.com.sg>
2018-08-25 22:58:04 +0800

    add Exe And Ohs Kata

---
+++ Kata/Codewars/ExeAndOhs.cs
@@ -0,0 +1,19 @@
+﻿using System.Linq;
+
+namespace Codewars
+{
+    internal class ExeAndOhs
+    {
+        public static bool XO(string input)
+        {
+            var xNum = CountNum(input, 'x');
+            var oNum = CountNum(input, 'o');
+            return xNum == oNum;
+        }
+
+        private static int CountNum(string input, char target)
+        {
+            return input.Count(x => x == target || x == char.ToUpper(target));
+        }
+    }
+}
