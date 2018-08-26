d185ed8fbd5f09c185722eccfe4d87dc08d06700
Eric Lin <ttttt123465@gmail.com>
2018-08-26 20:31:58 +0800

    finish digital cypher

---
+++ Kata/Codewars/DigitalCypher.cs
@@ -0,0 +1,22 @@
+﻿namespace Codewars
+{
+    public class DigitalCypher
+    {
+        public static int[] Encode(string str, int n)
+        {
+            var nums = new int[str.Length];
+            for (int i = 0; i < str.Length; i++)
+            {
+                nums[i] = str[i] - 96 + GetKeyValue(i, n);
+            }
+            return nums;
+        }
+
+        private static int GetKeyValue(int index, int key)
+        {
+            var keyString = key.ToString();
+            var keyIndex = index % keyString.Length;
+            return keyString[keyIndex]-48;
+        }
+    }
+}
---
+++ Kata/Kata_test/DigitalCypher_test.cs
@@ -0,0 +1,17 @@
+﻿using Codewars;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
+
+namespace Kata_test
+{
+    [TestFixture()]
+    public class DigitalCypher_test
+    {
+        [TestCase("scout", 1939, ExpectedResult = new[] { 20, 12, 18, 30, 21 })]
+        [TestCase("masterpiece", 1939, ExpectedResult = new int[] { 14, 10, 22, 29, 6, 27, 19, 18, 6, 12, 8 })]
+        public int[] TestMethod1(string str,int key)
+        {
+            return DigitalCypher.Encode(str,key);
+        }
+    }
+}
